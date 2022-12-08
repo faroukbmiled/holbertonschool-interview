0936612a5a4bcd2aa4961c6487f74f6be1dfc192
faroukbmiled <fcfaroukfc@gmail.com>
2022-12-08 16:26:33 +0100

    attempt

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 """check if all boxes can be opened"""
 
+
 def canUnlockAll(boxes):
+    """canUnlockAll function"""
     done = []
     keys = [0]
 
-    while len(keys) > 0:
-        key = keys.pop()
+    for key in keys:
         if key not in done:
             done.append(key)
-            keys.extend(boxes[key])
+            for box in boxes[key]:
+                if box not in keys:
+                    keys.append(box)
 
     if len(done) == len(boxes):
         return True
