60844742cb5bcc614a74369322fe9fa0213b369c
faroukbmiled <fcfaroukfc@gmail.com>
2022-12-18 21:35:21 +0100

    betty'

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -4,34 +4,35 @@
 
 listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *new_node, *current;
+	listint_t *new_node, *current;
 
-    if (!head)
-        return NULL;
-    new_node = malloc(sizeof(listint_t));
-    if (!new_node)
-        return NULL;
-    new_node->n = number;
-    new_node->next = NULL;
-    if (*head == NULL)
-    {
-        *head = new_node;
-        return new_node;
-    }
-    current = *head;
-    if (current->n >= number)
-    {
-        new_node->next = current;
-        *head = new_node;
-        return new_node;
-    }
-    
-    while (current->next != NULL && current->next->n < number)
-    {
-        current = current->next;
-    }
+	if (!head)
+		return (NULL);
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+		return (NULL);
+	new_node->n = number;
+	new_node->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
 
-    new_node->next = current->next;
-    current->next = new_node;
-    return new_node;
+	current = *head;
+	if (current->n >= number)
+	{
+		new_node->next = current;
+		*head = new_node;
+		return (new_node);
+	}
+
+	while (current->next != NULL && current->next->n < number)
+	{
+		current = current->next;
+	}
+
+	new_node->next = current->next;
+	current->next = new_node;
+	return (new_node);
 }
