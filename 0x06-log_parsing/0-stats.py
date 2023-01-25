828b7a90c741de929df64bc55d2d089a531a9f97
faroukbmiled <fcfaroukfc@gmail.com>
2023-01-26 00:49:58 +0100

    fix

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -10,12 +10,13 @@ def metrics(total_size, status_codes):
         print("{}: {}".format(status_code, status_codes[status_code]))
 
 
+size = 0
+status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+count = 0
+
 if __name__ == "__main__":
     """ main """
 
-    size = 0
-    status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-    count = 0
     try:
         while True:
             line = input()
