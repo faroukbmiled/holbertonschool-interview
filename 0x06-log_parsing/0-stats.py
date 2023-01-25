2808178869f717d18ad96398b876a412aeb45b98
faroukbmiled <fcfaroukfc@gmail.com>
2023-01-26 00:47:59 +0100

    fx

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -13,22 +13,22 @@ def metrics(total_size, status_codes):
 if __name__ == "__main__":
     """ main """
 
-size = 0
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-count = 0
-try:
-    while True:
-        line = input()
-        count += 1
-        elements = line.split()
-        if len(elements) != 9:
-            continue
-        status_code = int(elements[7])
-        file_size = int(elements[8])
-        if status_code in status_codes:
-            status_codes[status_code] += 1
-            size += file_size
-        if count % 10 == 0:
-            metrics(size, status_codes)
-except KeyboardInterrupt:
-    metrics(size, status_codes)
+    size = 0
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    count = 0
+    try:
+        while True:
+            line = input()
+            count += 1
+            elements = line.split()
+            if len(elements) != 9:
+                continue
+            status_code = int(elements[7])
+            file_size = int(elements[8])
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+                size += file_size
+            if count % 10 == 0:
+                metrics(size, status_codes)
+    except KeyboardInterrupt:
+        metrics(size, status_codes)
