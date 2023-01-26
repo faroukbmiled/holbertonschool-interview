5496cbabb988b608b9026049b3c5826914c172de
faroukbmiled <fcfaroukfc@gmail.com>
2023-01-26 19:19:18 +0100

    testing

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Script that read stdin line by line and computes metrics"""
+
+import sys
+
+size = 0
+status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+count = 0
+
+try:
+    for lines in sys.stdin:
+        try:
+            elements = lines.split(" ")
+            status_code = int(elements[7])
+            file_size = int(elements[8])
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+                size += file_size
+            count += 1
+        except Exception:
+            pass
+        if count % 10 == 0:
+            print('File size: {}'.format(size))
+            for status_code in sorted(status_codes.keys()):
+                print("{}: {}".format(status_code, status_codes[status_code]))
+            count = 0
+except KeyboardInterrupt:
+    print('File size: {}'.format(size))
+    for status_code in sorted(status_codes.keys()):
+        print("{}: {}".format(status_code, status_codes[status_code]))
+    raise
