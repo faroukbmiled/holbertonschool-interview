1cd5d72571f4836612123f0409bf3eb110c3011e
faroukbmiled <fcfaroukfc@gmail.com>
2023-04-13 22:20:16 +0100

    star wars api

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -5,22 +5,22 @@ const movieId = process.argv[2];
 const url = `https://swapi.dev/api/films/${movieId}/`;
 
 request(url, async (error, response, body) => {
-    if (error) {
-        console.error(error);
-    } else {
-        const characters = JSON.parse(body).characters;
-        for (const characterUrl of characters) {
-            await new Promise((resolve, reject) => {
-                request(characterUrl, (error, response, body) => {
-                    if (error) {
-                        console.error(error);
-                    } else {
-                        const character = JSON.parse(body);
-                        console.log(character.name);
-                    }
-                    resolve();
-                });
-            });
-        }
+  if (error) {
+    console.error(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (const characterUrl of characters) {
+      await new Promise((resolve, reject) => {
+        request(characterUrl, (error, response, body) => {
+          if (error) {
+            console.error(error);
+          } else {
+            const character = JSON.parse(body);
+            console.log(character.name);
+          }
+          resolve();
+        });
+      });
     }
+  }
 });
