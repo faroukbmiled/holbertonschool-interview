fbc0825c2da2819d239d9f4ae6a5240fa102214d
faroukbmiled <fcfaroukfc@gmail.com>
2023-04-27 23:54:15 +0100

    req

--- 0x11-heap_sort/sort.h
+++ 0x11-heap_sort/sort.h
@@ -1,12 +1,11 @@
-#ifndef MAIN_h
-#define MAIN_H
+#ifndef SORT_H
+#define SORT_H
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <stddef.h>
 
 void print_array(const int *array, size_t size);
-void heap_sort(int *array, size_t size);
+void swap(int *a, int *b);
 void sift_down(int *array, size_t start, size_t end, size_t size);
+void heap_sort(int *array, size_t size);
 
 #endif
