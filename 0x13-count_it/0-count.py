f869103d673197be18b64bea2ea16aca2a915eef
faroukbmiled <fcfaroukfc@gmail.com>
2023-05-11 23:35:01 +0100

    fix

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -3,33 +3,39 @@
 import requests
 
 
-def count_words(subreddit, word_list, after=None, counts=None):
-    """begin func"""
-    if counts is None:
-        counts = {}
+def count_words(subreddit, word_list, after=None, count=None):
+    """count using reddit api"""
 
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    params = {"limit": 100, "after": after}
-    response = requests.get(url, headers=headers, params=params)
+    if count == None:
+        count = {}
+    if after == None:
+        url = "https://www.reddit.com/r/{}/hot.json".format(
+            subreddit)
+    else:
+        url = "https://www.reddit.com/r/{}/hot.json?after={}".format(
+            subreddit, after)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
 
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
-        articles = data["data"]["children"]
-
-        for article in articles:
-            title = article["data"]["title"].lower()
-
+        posts = data['data']['children']
+        for post in posts:
+            title = post['data']['title']
+            words = title.lower().split()
             for word in word_list:
-                if word.lower() in title:
-                    counts[word] = counts.get(
-                        word, 0) + title.count(word.lower())
+                if word.lower() in words:
+                    count[word] = count.get(word, 0) + words.count(
+                        word.lower())
 
-        after = data["data"]["after"]
+        after = data['data']['after']
         if after is not None:
-            return count_words(subreddit, word_list, after, counts)
+            return count_words(
+                subreddit, word_list, after, count)
+    else:
+        pass
 
-    sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
-
+    sorted_counts = sorted(count.items(), key=lambda x: (-x[1], x[0]))
     for word, count in sorted_counts:
-        print("{}: {}".format(word, count))
+        print("{}: {}".format(word.lower(), count))
