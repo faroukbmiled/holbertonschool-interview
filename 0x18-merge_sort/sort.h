410d20e032ede93199a815a53a3d73b6ae2a00de
faroukbmiled <fcfaroukfc@gmail.com>
2023-06-22 21:52:51 +0100

    add

--- 0x18-merge_sort/sort.h
+++ 0x18-merge_sort/sort.h
@@ -6,7 +6,5 @@
 
 void print_array(const int *array, size_t size);
 void merge_sort(int *array, size_t size);
-void split(int *arr, int first, int last, int *sorted_arr);
-void merge(int *arr, int start, int mid, int end, int *copy);
 
 #endif /* SORT_H */
