b27eb65058ae083fc4b07ce19a5989c469b8c0da
faroukbmiled <fcfaroukfc@gmail.com>
2024-01-29 12:31:03 +0100

    prime game

---
+++ primegame/0-prime_game.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Prime Game Module"""
+
+
+def is_prime(n):
+    """is_prime: Check if a number is prime"""
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+def isWinner(x, nums):
+    """
+    isWinner: Determines the winner of the prime game for each round.
+    """
+    if not nums or x <= 0:
+        return None
+
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        prime_count = sum(1 for i in range(1, n + 1) if is_prime(i))
+        if prime_count % 2 == 0:
+            ben_wins += 1
+        else:
+            maria_wins += 1
+
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
