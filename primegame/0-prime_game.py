b410cc6447ae9958158cd11e29f1fd78c92a1e11
faroukbmiled <fcfaroukfc@gmail.com>
2024-01-30 23:23:45 +0100

    doc

--- primegame/0-prime_game.py
+++ primegame/0-prime_game.py
@@ -3,6 +3,7 @@
 
 
 def is_prime_number(n):
+    """is_prime_number"""
     for i in range(2, int(n ** 0.5) + 1):
         if not n % i:
             return False
@@ -10,6 +11,7 @@ def is_prime_number(n):
 
 
 def add_prime_number(n, primes_list):
+    """add_prime_number"""
     last_prime = primes_list[-1]
     if n > last_prime:
         for i in range(last_prime + 1, n + 1):
@@ -20,11 +22,13 @@ def add_prime_number(n, primes_list):
 
 
 def determine_round_winner(round_number, primes_list):
+    """determine_round_winner"""
     prime_count = sum((i != 0 and i <= round_number) for i in primes_list[:round_number + 1])
     return "Maria" if prime_count % 2 else "Ben"
 
 
 def isWinner(num_rounds, round_values):
+    """isWinner"""
     players_score = {"Maria": 0, "Ben": 0}
     primes = [0, 0, 2]
     add_prime_number(max(round_values), primes)
