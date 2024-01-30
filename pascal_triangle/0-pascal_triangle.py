3e0cdb9633916b25b0d13c6c6f8fc94416abe15e
faroukbmiled <fcfaroukfc@gmail.com>
2024-01-30 22:23:44 +0100

    done

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""pascal_triangle"""
+
+def pascal_triangle(n):
+    """pascal_triangle func"""
+    if n <= 0:
+        return []
+
+    triangle = []
+    for i in range(n):
+        row = [1]
+        if triangle:
+            last_row = triangle[-1]
+            row.extend([sum(pair) for pair in zip(last_row, last_row[1:])])
+            row.append(1)
+        triangle.append(row)
+
+    return triangle
