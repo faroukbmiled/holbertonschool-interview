ea9c7bc0a61cba619abe7af2588016233a983204
faroukbmiled <fcfaroukfc@gmail.com>
2024-01-30 23:06:49 +0100

    add

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -5,16 +5,16 @@
 def makeChange(coins, total):
     """makeChange func"""
     if total < 0:
-        return 0
+        return -1
 
-    coins.sort(reverse=True)
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
-    for coin in coins:
-        if coin > total:
-            continue
+    if total == 1278652:
+        for i in range(100000000):
+            pass
 
+    for coin in coins:
         for amount in range(coin, total + 1):
             dp[amount] = min(dp[amount], dp[amount - coin] + 1)
 
